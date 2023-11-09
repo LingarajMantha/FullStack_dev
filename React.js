ae82970eabc63dd5e52382dd8f4e2a6293c99a10
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-11-10 04:53:46 +0530

    Create React.js

---
+++ React.js
@@ -0,0 +1,5 @@
+React js is usually used for the better user experience , like when we are moivng from one page to another 
+like www.google.com to www.google.com/blog , so usually before page used to load/buffer while moving from one page to other ,,, 
+  now if we try to move with React.js , it wont even load, HENCE it is Single page loading ,, which gives people a better user experinece.
+
+  Syllabus to complete in the React JS topics  - > 
