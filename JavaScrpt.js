25bc1477f22635a0ae5c9ab2645573c7fa52495c
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-11-10 05:17:05 +0530

    Update JavaScrpt.js

--- JavaScrpt.js
+++ JavaScrpt.js
@@ -1,1 +1,11 @@
+Advanced JS 
+ES5 - ES6 
+arrays, objects, functions
+arrow functions/normal functions
+array methods (map , filter, reduce, find, findIndex)
+rest spread
+try catch
+  async await
+  promises
+
 
