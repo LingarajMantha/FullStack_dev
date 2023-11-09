d04b97dfe5d5117ceaa6bd24d1687efcdcd161ff
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-11-10 05:02:22 +0530

    Update React.js

--- React.js
+++ React.js
@@ -2,4 +2,11 @@ React js is usually used for the better user experience , like when we are moivn
 like www.google.com to www.google.com/blog , so usually before page used to load/buffer while moving from one page to other ,,, 
   now if we try to move with React.js , it wont even load, HENCE it is Single page loading ,, which gives people a better user experinece.
 
-  Syllabus to complete in the React JS topics  - > 
+Features of ReactJS
+->Its a javascript libraray 
+-> used to build single page application and helps developer to build the web page based on compononent wise (header is on componenet , footer and Body differenlty , usefull for developer)
+->when ever an user clicks on some componenet in the UI , only that part loads , like while useing the insta nd fb , if we click on some componenet 
+only that will change, not the whole page 
+->React js is view layer of MVC - (model view controller)
+
+
